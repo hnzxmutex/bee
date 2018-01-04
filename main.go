927acf7d7bb35a2acd0ab8a2611c80a3fed0477a
hnzxmutex <tsengmeat@gmail.com>
2018-01-04 20:35:48 +0800

    fix path bug

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ import (
 	"log"
 	"os"
 
+	"github.com/beego/bee/cmd/commands"
 	"github.com/beego/bee/config"
 	"github.com/beego/bee/generate/swaggergen"
 	"github.com/beego/bee/utils"
 	"github.com/hnzxmutex/bee/cmd"
-	"github.com/hnzxmutex/bee/cmd/commands"
 )
 
 func main() {
